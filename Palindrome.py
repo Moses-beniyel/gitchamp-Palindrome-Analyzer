034c0b3b2a734ae6a4392ffed824c78069239498
Moses-beniyel <mosesbeniyel007@gmail.com>
2025-09-13 13:42:34 +0530

    palindome

--- Palindrome.py
+++ Palindrome.py
@@ -1,1 +1,15 @@
-
+def checkpalindrome(s):
+   
+    l=0
+    r=len(s)-1
+    while l<r:
+        if s[l]!=s[r]:
+            return False
+        l+=1
+        r-=1
+    return True
+s=input("Enter a string: ")
+if checkpalindrome(s):
+    print("The given string is palindrome")
+else:
+    print("it is not a palindome")
