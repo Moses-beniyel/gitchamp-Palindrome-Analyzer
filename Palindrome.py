151f772fcecc679c2ba9c1e10976068dbf979c84
Moses-beniyel <mosesbeniyel007@gmail.com>
2025-09-13 13:58:50 +0530

    moses

--- Palindrome.py
+++ Palindrome.py
@@ -15,12 +15,15 @@ else:
     print("it is not a palindome")
 
 
-def checkpalindrome(s):
-    l=0
-    r=len(s)-1
-    while l<r:
-        if s[l]!=s[r]:
-            return False
-        l+=1
-        r-=1
-    return True
+def checklongestpalindrome(arr):
+    maxi=-1
+    for i in arr:
+        if checkpalindrome(i):
+            if(len(i)>maxi):
+                maxi=len(i)
+                res=i
+    return res
+
+arr=["moses","madam","noon","racecar","python"]
+
+print("The longest palindrome is:",checklongestpalindrome(arr))
