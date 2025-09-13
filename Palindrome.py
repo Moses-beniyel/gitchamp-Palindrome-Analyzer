054d688cb79ed7780193165668cd420b64168fd7
Moses-beniyel <mosesbeniyel007@gmail.com>
2025-09-13 13:55:45 +0530

    moses

--- Palindrome.py
+++ Palindrome.py
@@ -15,4 +15,12 @@ else:
     print("it is not a palindome")
 
 
-hello
+def checkpalindrome(s):
+    l=0
+    r=len(s)-1
+    while l<r:
+        if s[l]!=s[r]:
+            return False
+        l+=1
+        r-=1
+    return True
